d631c8313798f0028fc03782371af35e9fa10acd
Yudho Panji Al Akbar <yudhopanjialakbar@gmail.com>
2026-09-16 23:45:46 +0700

    Commit pertama: membuat program dasar penghitung panen

---
+++ panen.py
@@ -0,0 +1,5 @@
+# Program sederhana menghitung total hasil panen
+data_panen = [120, 95, 150, 80, 110] # dalam kilogram
+
+total_panen = sum(data_panen)
+print(f"Total hasil panen saat ini adalah: {total_panen} kg")
