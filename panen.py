b79e88cf1bfa25b58109c37dad4d59827daec2d0
Yudho Panji Al Akbar <yudhopanjialakbar@gmail.com>
2026-09-16 23:49:15 +0700

    Menambahkan fitur perhitungan diskon

--- panen.py
+++ panen.py
@@ -3,3 +3,7 @@ data_panen = [120, 95, 150, 80, 110] # dalam kilogram
 
 total_panen = sum(data_panen)
 print(f"Total hasil panen saat ini adalah: {total_panen} kg")
+
+# Fitur diskon (Soal 2b)
+diskon = total_panen * 0.10 # diskon 10%
+print(f"Diskon yang diberikan seberat: {diskon} kg")
